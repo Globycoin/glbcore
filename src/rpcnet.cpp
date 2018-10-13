3a164b73e08c33de16ac67abcbeee8452585d8ae
Globycoin <40317008+Globycoin@users.noreply.github.com>
2018-10-13 13:59:19 +0200

    Update rpcnet.cpp

--- src/rpcnet.cpp
+++ src/rpcnet.cpp
@@ -170,7 +170,7 @@ Value addnode(const Array& params, bool fHelp)
             "1. \"node\"     (string, required) The node (see getpeerinfo for nodes)\n"
             "2. \"command\"  (string, required) 'add' to add a node to the list, 'remove' to remove a node from the list, 'onetry' to try a connection to the node once\n"
             "\nExamples:\n" +
-            HelpExampleCli("addnode", "\"192.168.0.6:40001\" \"onetry\"") + HelpExampleRpc("addnode", "\"192.168.0.6:23500\", \"onetry\""));
+            HelpExampleCli("addnode", "\"192.168.0.6:23500\" \"onetry\"") + HelpExampleRpc("addnode", "\"192.168.0.6:23500\", \"onetry\""));
 
     string strNode = params[0].get_str();
 
