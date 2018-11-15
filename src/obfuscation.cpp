d5dd9b5475915956849658373d8658286a08781b
Globycoin <40317008+Globycoin@users.noreply.github.com>
2018-11-15 01:42:39 +0100

    2.1.1.1

--- src/obfuscation.cpp
+++ src/obfuscation.cpp
@@ -2111,7 +2111,6 @@ bool CObfuScationSigner::IsVinAssociatedWithPubkey(CTxIn& vin, CPubKey& pubkey)
     uint256 hash;
     if (GetTransaction(vin.prevout.hash, txVin, hash, true)) {
         BOOST_FOREACH (CTxOut out, txVin.vout) {
-         //   if (out.nValue == MASTERNODE_COLLATERAL(chainActive.Height()) * COIN) {
             if (out.nValue == MASTERNODE_COLLATERAL) {
                 if (out.scriptPubKey == payee2) return true;
             }
